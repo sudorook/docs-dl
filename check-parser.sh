18ca1b766ec2233ff40ef1c2dc01cfb5a6c1ac9a
sudorook <daemon@nullcodon.com>
2022-04-03 03:04:08 -0500

    add script to test that parse works for all cases

---
+++ check-parser.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -euo pipefail
+source globals
+source data
+source ./get-refs.sh
+
+function test_parser {
+  local version
+  local key
+  local all_good=true
+  local cmd
+
+  for key in "${!CMD[@]}"; do
+    cmd="${CMD[${key}]}"
+    if version="$(eval "${cmd}" 2>/dev/null | parse_version)"; then
+      if ! [[ "${version}" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
+        all_good=false
+        show_error "✗ ${key}"
+      else
+        show_success "✓ ${key}"
+      fi
+    fi
+  done
+
+  ${all_good} && return 0 || return 1
+}
+
+test_parser
