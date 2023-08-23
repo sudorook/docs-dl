8bd08481cad2adfa161e3714aed072fb14c6313d
sudorook <daemon@nullcodon.com>
2023-08-23 11:34:32 -0500

    adjust check_parsers.sh to check parser(s) specified in cli only

--- check-parser.sh
+++ check-parser.sh
@@ -6,30 +6,49 @@ source ./get-refs.sh
 
 function test_parser {
   local version
-  local key
-  local all_good=true
+  local key="${1}"
   local cmd
 
-  for key in "${!CMD[@]}"; do
-    cmd="${CMD["${key}"]}"
-    if version="$(eval "${cmd}" 2> /dev/null | parse_version)"; then
-      if ! [[ "${version}" =~ ^[0-9]+(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
-        all_good=false
-        show_error "✗ ${key}"
-      else
-        show_success "✓ ${key}"
-      fi
+  cmd="${CMD["${key}"]}"
+  if version="$(eval "${cmd}" 2> /dev/null | parse_version)"; then
+    if ! [[ "${version}" =~ ^[0-9]+(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
+      show_error "✗ ${key}"
+      return 1
     else
-      if command -v "${key%% *}" > /dev/null; then
-        all_good=false
-        show_error "✗ ${key}"
-      else
-        show_warning "？${key}"
-      fi
+      show_success "✓ ${key}"
+      return 0
     fi
-  done
-
-  ${all_good} && return 0 || return 1
+  else
+    if command -v "${key%% *}" > /dev/null; then
+      show_error "✗ ${key}"
+      return 1
+    else
+      show_warning "？${key}"
+      return 0
+    fi
+  fi
 }
 
-test_parser
+ALL_GOOD=true
+
+if [ "${0}" = "${BASH_SOURCE[0]}" ]; then
+  if [ "${#}" -eq 0 ]; then
+    for KEY in "${!CMD[@]}"; do
+      if ! test_parser "${KEY}"; then
+        ALL_GOOD=false
+      fi
+    done
+  else
+    for KEY in "${@}"; do
+      if [ -n "${CMD[${KEY}]-}" ]; then
+        if ! test_parser "${KEY}"; then
+          ALL_GOOD=false
+        fi
+      else
+        show_error "ERROR: ${KEY@Q} not found."
+      fi
+    done
+  fi
+fi
+
+"${ALL_GOOD}" && exit 0 || exit 1
