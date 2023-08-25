024062ddf758d53d79dd65362423a539dff51999
sudorook <daemon@nullcodon.com>
2023-08-24 20:19:36 -0500

    allow urls to be checked on per-key basis

--- check-urls.sh
+++ check-urls.sh
@@ -9,42 +9,60 @@ function try_wget {
   wget -q --tries=5 --timeout=30 --spider "${1}" > /dev/null
 }
 
-function test_urls {
+function test_url {
   local version
   local url
   local urls
   local tmp
-  local key
-  local is_missing=false
+  local key="${1}"
+  local is_good=true
 
-  for key in "${!TEST[@]}"; do
-    IFS='.' read -r -a version <<< "${TEST["${key}"]}"
-    IFS=' ' read -r -a urls <<< "${URL["${key}"]}"
-    for tmp in "${urls[@]}"; do
-      url="${tmp}"
-      if [ "${#version[@]}" -eq 3 ]; then
-        url="${url//MAJOR/${version[0]}}"
-        url="${url//MINOR/${version[1]}}"
-        url="${url//PATCH/${version[2]}}"
-      elif [ "${#version[@]}" -eq 2 ]; then
-        url="${url//MAJOR/${version[0]}}"
-        url="${url//MINOR/${version[1]}}"
-        url="${url//.PATCH/}"
-      elif [ "${#version[@]}" -eq 1 ]; then
-        url="${url//MAJOR/${version[0]}}"
-        url="${url//.MINOR/}"
-        url="${url//.PATCH/}"
-      fi
-      if try_wget "${url}"; then
-        show_success "${key}: ${url}"
-      else
-        show_error "${key}: ${url}"
-        is_missing=true
-      fi
-    done
+  IFS='.' read -r -a version <<< "${TEST["${key}"]}"
+  IFS=' ' read -r -a urls <<< "${URL["${key}"]}"
+  for tmp in "${urls[@]}"; do
+    url="${tmp}"
+    if [ "${#version[@]}" -eq 3 ]; then
+      url="${url//MAJOR/${version[0]}}"
+      url="${url//MINOR/${version[1]}}"
+      url="${url//PATCH/${version[2]}}"
+    elif [ "${#version[@]}" -eq 2 ]; then
+      url="${url//MAJOR/${version[0]}}"
+      url="${url//MINOR/${version[1]}}"
+      url="${url//.PATCH/}"
+    elif [ "${#version[@]}" -eq 1 ]; then
+      url="${url//MAJOR/${version[0]}}"
+      url="${url//.MINOR/}"
+      url="${url//.PATCH/}"
+    fi
+    if try_wget "${url}"; then
+      show_success "${key}: ${url}"
+    else
+      show_error "${key}: ${url}"
+      is_good=false
+    fi
   done
 
-  ${is_missing} && return 1 || return 0
+  "${is_good}" && return || return 1
 }
 
-test_urls
+if [ "${0}" = "${BASH_SOURCE[0]}" ]; then
+  ALL_GOOD=true
+  if [ "${#}" -eq 0 ]; then
+    for KEY in "${!TEST[@]}"; do
+      if ! test_url "${KEY}"; then
+        ALL_GOOD=false
+      fi
+    done
+  else
+    for KEY in "${@}"; do
+      if [ -n "${CMD[${KEY}]-}" ]; then
+        if ! test_url "${KEY}"; then
+          ALL_GOOD=false
+        fi
+      else
+        show_error "ERROR: ${KEY@Q} not found."
+      fi
+    done
+  fi
+  "${ALL_GOOD}" && exit 0 || exit 1
+fi
